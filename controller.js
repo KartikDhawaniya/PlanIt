28ec19922a25961ad5f915c410b6539edd1d309e
Kartik Dhawaniya <200050060@iitb.ac.in>
2023-04-10 23:37:40 +0530

    Testing env vars

--- controller.js
+++ controller.js
@@ -1,6 +1,32 @@
 const express = require('express');
-const app = express();
 const process = require('process');
+const { Client } = require('pg');
+const keys = require('./config.json');
+const app = express();
+
+const client = new Client({
+    host: process.env.host,
+    port: process.env.port,
+    dbname: process.env.database,
+    user: process.env.user,
+    password: process.env.password,
+    sslmode: process.env.sslmode,
+});
+
+console.log('Connecting to Azure PostgreSQL database');
+
+// client.connect()
+//     .then(() => {
+//         console.log('Connected to Azure PostgreSQL database');
+//         // Perform database operations here
+//     })
+//     .catch(error => {
+//         console.error('Failed to connect to Azure PostgreSQL database:', error);
+//     });
+
+console.log('Env variables: ')
+console.log(client);
+
 
 
 port = process.env.PORT || 3000;
